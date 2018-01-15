010d85b3e9772911dbd9e729f652f4c5f355cd38
michael-kotliar <misha.kotliar@gmail.com>
2018-01-14 19:35:25 -0500

    Closes #4, display RNA-Seq bigWigs
    For bedGraph the original SELECT is
    used. For bigWig (not dUTP) _f_wtrack
    is added. For bigWig dUTP, container
    multiWig is added with upstream_f_wtrack
    and downstream_f_wtrack. If both up-
    and down-stream bigwig files are positive
    and displayed with overlay, autoscale
    works correct. If one of the bigWig file's
    data is negative, autoscale works
    independently for each of the bigWig file
    so result is difficult to compare

--- sql_patch/biowardrobe_patched_view/xenTro9_view_patch.sql
+++ sql_patch/biowardrobe_patched_view/xenTro9_view_patch.sql
@@ -32,6 +32,141 @@ WHERE ((`l`.`deleted` = 0)
        AND (`l`.`genome_id` = `g`.`id`)
        AND (`g`.`db` LIKE 'xenTro9%'))
 UNION
+SELECT replace(concat(`l`.`uid`,'_f_wtrack'),'-','_') AS `tableName`,
+       `l`.`name4browser` AS `shortLabel`,
+       'bigWig' AS `type`,
+       `l`.`name4browser` AS `longLabel`,
+       2 AS `visibility`,
+       2 AS `priority`,
+       30 AS `colorR`,
+       70 AS `colorG`,
+       150 AS `colorB`,
+       30 AS `altColorR`,
+       70 AS `altColorG`,
+       150 AS `altColorB`,
+       0 AS `useScore`,
+       0 AS `private`,
+       0 AS `restrictCount`,
+       NULL AS `restrictList`,
+       NULL AS `url`,
+       NULL AS `html`,
+       `l`.`egroup_id` AS `grp`,
+       0 AS `canPack`,
+       concat('track ',replace(concat(`l`.`uid`,'_f_wtrack'),'-','_'),'
+               autoScale on
+               visibility full
+               windowingFunction maximum') AS `settings`
+FROM ((`ems`.`labdata` `l`
+       JOIN `ems`.`experimenttype` `et`)
+       JOIN `ems`.`genome` `g`)
+WHERE ((`l`.`deleted` = 0)
+       AND (`l`.`libstatus` BETWEEN 10 AND 99)
+       AND (`l`.`experimenttype_id` = `et`.`id`)
+       AND (`et`.`etype` LIKE '%RNA%')
+       AND (`et`.`etype` NOT LIKE '%dUTP%')
+       AND (`l`.`genome_id` = `g`.`id`)
+       AND (`g`.`db` LIKE 'xenTro9%'))
+UNION
+SELECT replace(concat(`l`.`uid`,'_grp'),'-','_') AS `tableName`,
+       `l`.`name4browser` AS `shortLabel`,
+       'bigWig' AS `type`,
+       `l`.`name4browser` AS `longLabel`,
+       2 AS `visibility`,
+       2 AS `priority`,
+       30 AS `colorR`,
+       70 AS `colorG`,
+       150 AS `colorB`,
+       30 AS `altColorR`,
+       70 AS `altColorG`,
+       150 AS `altColorB`,
+       0 AS `useScore`,
+       0 AS `private`,
+       0 AS `restrictCount`,
+       NULL AS `restrictList`,
+       NULL AS `url`,
+       NULL AS `html`,
+       `l`.`egroup_id` AS `grp`,
+       0 AS `canPack`,
+       concat('track ',replace(concat(`l`.`uid`,'_grp'),'-','_'),'
+               group ',`l`.`egroup_id`,'
+               container multiWig
+               autoScale on
+               visibility full
+               windowingFunction maximum') AS `settings`
+FROM ((`ems`.`labdata` `l`
+       JOIN `ems`.`experimenttype` `et`)
+       JOIN `ems`.`genome` `g`)
+WHERE ((`l`.`deleted` = 0)
+       AND (`l`.`libstatus` BETWEEN 10 AND 99)
+       AND (`l`.`experimenttype_id` = `et`.`id`)
+       AND (`et`.`etype` LIKE '%RNA%dUTP%')
+       AND (`l`.`genome_id` = `g`.`id`)
+       AND (`g`.`db` LIKE 'xenTro9%'))
+UNION
+SELECT replace(concat(`l`.`uid`,'_upstream_f_wtrack'),'-','_') AS `tableName`,
+       `l`.`name4browser` AS `shortLabel`,
+       'bigWig' AS `type`,
+       `l`.`name4browser` AS `longLabel`,
+       2 AS `visibility`,
+       2 AS `priority`,
+       30 AS `colorR`,
+       70 AS `colorG`,
+       150 AS `colorB`,
+       30 AS `altColorR`,
+       150 AS `altColorG`,
+       70 AS `altColorB`,
+       0 AS `useScore`,
+       0 AS `private`,
+       0 AS `restrictCount`,
+       NULL AS `restrictList`,
+       NULL AS `url`,
+       NULL AS `html`,
+       `l`.`egroup_id` AS `grp`,
+       0 AS `canPack`,
+       concat('track ',replace(concat(`l`.`uid`,'_upstream_f_wtrack'),'-','_'),'
+               parent ',replace(concat(`l`.`uid`,'_grp'),'-','_')) AS `settings`
+FROM ((`ems`.`labdata` `l`
+       JOIN `ems`.`experimenttype` `et`)
+       JOIN `ems`.`genome` `g`)
+WHERE ((`l`.`deleted` = 0)
+       AND (`l`.`libstatus` BETWEEN 10 AND 99)
+       AND (`l`.`experimenttype_id` = `et`.`id`)
+       AND (`et`.`etype` LIKE '%RNA%dUTP%')
+       AND (`l`.`genome_id` = `g`.`id`)
+       AND (`g`.`db` LIKE 'xenTro9%'))
+UNION
+SELECT replace(concat(`l`.`uid`,'_downstream_f_wtrack'),'-','_') AS `tableName`,
+       `l`.`name4browser` AS `shortLabel`,
+       'bigWig' AS `type`,
+       `l`.`name4browser` AS `longLabel`,
+       2 AS `visibility`,
+       2 AS `priority`,
+       30 AS `colorR`,
+       70 AS `colorG`,
+       150 AS `colorB`,
+       30 AS `altColorR`,
+       150 AS `altColorG`,
+       70 AS `altColorB`,
+       0 AS `useScore`,
+       0 AS `private`,
+       0 AS `restrictCount`,
+       NULL AS `restrictList`,
+       NULL AS `url`,
+       NULL AS `html`,
+       `l`.`egroup_id` AS `grp`,
+       0 AS `canPack`,
+       concat('track ',replace(concat(`l`.`uid`,'_downstream_f_wtrack'),'-','_'),'
+               parent ',replace(concat(`l`.`uid`,'_grp'),'-','_')) AS `settings`
+FROM ((`ems`.`labdata` `l`
+       JOIN `ems`.`experimenttype` `et`)
+       JOIN `ems`.`genome` `g`)
+WHERE ((`l`.`deleted` = 0)
+       AND (`l`.`libstatus` BETWEEN 10 AND 99)
+       AND (`l`.`experimenttype_id` = `et`.`id`)
+       AND (`et`.`etype` LIKE '%RNA%dUTP%')
+       AND (`l`.`genome_id` = `g`.`id`)
+       AND (`g`.`db` LIKE 'xenTro9%'))
+UNION
 SELECT replace(concat(`l`.`uid`,'_grp'),'-','_') AS `tableName`,
        `l`.`name4browser` AS `shortLabel`,
        'bed 4 +' AS `type`,
