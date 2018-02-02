d27b280d69259a1931a68446f4e67bc5e47701ba
Michael Kotliar <misha.kotliar@gmail.com>
2018-02-01 22:38:08 -0500

    Tested

--- basic_analysis/single_test.py
+++ basic_analysis/single_test.py
@@ -5,5 +5,5 @@ import single_rna_dna_upload
 
 biow_db_settings = Settings()
 print single_rna_get_job.get_job(id, biow_db_settings.conn)
-print single_dna_get_job(id, biow_db_settings.conn)
+print single_dna_get_job.get_job(id, biow_db_settings.conn)
 print single_rna_dna_upload.upload_job(id, biow_db_settings.conn)
